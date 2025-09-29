fee19560fe511d8138b3f5016bfeca34830c0c08
iamkapilll <kapildevchaudhary067@gmail.com>
2025-09-29 20:39:28 +0545

    schema for message

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,6 +1,18 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface Message extends Document {  //an interface mande for "Message"
-    content: string;
+    content: string; //string in small for typescript
     createdAt: Date;
 }
+
+const MessageSchema: Schema<Message> = new mongoose.Schema({   // so if we have an interface designed for "schema" as defined in line 3, we have used this as: "Schema<Message>" as the Schema in line 3 was interface for messageschema, but here inthe same place if this would have be interface String, here it will be this: Schema<String>
+    content: {
+        type: String, //String in Capital letter for mongoose
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    }
+})
