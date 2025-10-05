48e4e5484742c4acb3f5cbcdf86dd3058d2a354a
iamkapilll <kapildevchaudhary067@gmail.com>
2025-10-05 20:50:18 +0545

    verifyschema with zod

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,1 +1,8 @@
 //verifySchema..
+import { z } from 'zod'
+
+export const verifySchema = z.object({
+    code: z
+    .string()
+    .length(6, { message: 'verification code must be 6 characters long'})
+})
