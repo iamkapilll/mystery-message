62cf6fb19dc8129e103e1199639c907c30f1f3f9
iamkapilll <kapildevchaudhary067@gmail.com>
2025-10-09 00:24:46 +0545

    types defined for session

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,30 @@
+//13.1 kinda like simultaniously working with options.ts
+//src/types/next-auth.d.ts
+import 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      _id?: string;
+      isVerified?: boolean;
+      isAcceptingMessages?: boolean;
+      username?: string;
+    } & DefaultSession['user'];
+  }
+
+  interface User {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+  }
+}
