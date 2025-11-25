2291e3ddbe193982efdc087a03ea84dbe1716970
iamkapilll <kapildevchaudhary067@gmail.com>
2025-11-25 17:33:59 +0545

    verification

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -5,7 +5,6 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
 
-
 import { ApiResponse } from "@/types/ApiResponse";
 
 export async function sendVerificationEmail(  //This async function takes three arguments:
